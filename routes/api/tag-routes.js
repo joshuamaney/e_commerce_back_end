44788dbedbfe3f793f2b6557d3566c0d30bbeb33
Joshua Maney <joshuaraymaney@gmail.com>
2021-04-26 23:29:28 -0400

    added get function and fixed some mismatched quotes for continuity

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -8,7 +8,7 @@ router.get('/', async (req, res) => {
   // be sure to include its associated Product data
   try {
     const tagData = await Tag.findAll({
-      include: [{ model: Product, through: ProductTag, as: "products" }]
+      include: [{ model: Product, through: ProductTag, as: 'products' }]
     });
     res.status(200).json(tagData);
   } catch (err) {
@@ -19,6 +19,18 @@ router.get('/', async (req, res) => {
 router.get('/:id', async (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  try {
+    const tagData = await Tag.findByPk(req.params.id, {
+      include: [{ model: Product, through: ProductTag, as: 'products' }]
+    });
+    if (!tagData) {
+      res.status(404).json({ message: 'No tag found with that id!' });
+      return;
+    }
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 
 });
 
