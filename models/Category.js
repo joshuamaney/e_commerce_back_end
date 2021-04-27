3769bfe9707d046a62485f81b7f05b2d89f391c4
Joshua Maney <joshuaraymaney@gmail.com>
2021-04-26 23:54:46 -0400

    fixed the category.js

--- models/Category.js
+++ models/Category.js
@@ -13,31 +13,10 @@ Category.init(
       allowNull: false,
       autoIncrement: true
     },
-    product_name: {
+    category_name: {
       type: DataTypes.STRING,
       allowNull: false,
-    },
-    price: {
-      type: DataTypes.DECIMAL,
-      allowNull: false,
-      validate: {
-        isDecimal: true
-      }
-    },
-    stock: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      defaultValue: 10,
-      validate: {
-        isNumeric: true
-      }
-    },
-    category_id: {
-      type: DataTypes.INTEGER,
-      references: {
-        model: 'category',
-        key: 'id'
-      }
+      unique: true
     }
   },
   {
